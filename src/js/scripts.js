10c99c1b55babeb931a5198a179be7b46880f20f
Jacques Olivier <jacqueso.olivier@gmail.com>
2023-06-24 13:08:18 +1200

    chnages!

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -313,6 +313,7 @@ function animateCameraBackward() {
   }
 }
 
+
 const points = curve.getPoints(50);
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
 const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
