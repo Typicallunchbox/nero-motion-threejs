2fac4558dd4b191016ce3475d60281e3b12c376d
Keegan <keegan.launspach1@hotmail.com>
2023-06-25 14:32:26 +1200

    code cleanup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,58 +8,79 @@ import { isEqual } from 'lodash';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper';
 import * as TWEEN from '@tweenjs/tween.js';
-// import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader'
 
-// const hdrTextureURL = new URL('../img/hdri.hdr', import.meta.url)
+
+//BASIC VARIABLES
+var model = null;
+var start = { x: 0, y: 0 };
+var counter = 0;
+var currentIndex = 0;
+var childMesh = null;
+const clock = new THREE.Clock();
+const assetLoader = new GLTFLoader();
 const buildingURL = new URL('../assets/building.glb', import.meta.url);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-
-// renderer.outputEncoding = THREE.LinearEncoding;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-// renderer.physicallyCorrectLights = true;
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 const interpolatedPoints = [];
 const scene = new THREE.Scene();
+const stats = new Stats();
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
   10000,
 );
+const pointSets = [
+  {
+    startPoint: new THREE.Vector3(235, 162, 370),
+    endPoint: new THREE.Vector3(36.56, 12, 48.96),
+  },
+  {
+    startPoint: new THREE.Vector3(36.56, 12, 48.96),
+    endPoint: new THREE.Vector3(-11, 8, 49),
+    step: 50
+  },
+  {
+    startPoint: new THREE.Vector3(-11, 8, 49),
+    endPoint: new THREE.Vector3(-18, 7.7, 49.86),
+    step: 50
+  },
+  {
+    startPoint: new THREE.Vector3(-18, 7.7, 49.86),
+    endPoint: new THREE.Vector3(-30.8, 7.7, 55.2),
+    step: 50
+  },
+  {
+    startPoint: new THREE.Vector3(-30.8, 7.7, 55.2),
+    endPoint: new THREE.Vector3(-30, 7.4, 59.8),
+    
+  },
+  // Add more sets as needed
+];
+
+//Initializing canvas to the dom
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+gsap.registerPlugin(MotionPathPlugin);
 
 
-//CAMERA FREE MOVEMENT OPTIONS
-// const orbit = new OrbitControls(camera, renderer.domElement);
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.movementSpeed = 8;
-controls.lookSpeed = 0.08;
-
-//SHOWS AXES
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-//DEFAULT CAMERA POSITION
+//DEFAULTS
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 camera.position.set(235, 162, 370);
 camera.lookAt(11, 8, 49);
+
+
+//DEBUGGING CAMERA FREE MOVEMENT VARIABLES
+// const orbit = new OrbitControls(camera, renderer.domElement);
+// const controls = new FirstPersonControls(camera, renderer.domElement);
+// controls.movementSpeed = 8;
+// controls.lookSpeed = 0.08;
 // orbit.update();
 
-//WAS NOT PROVIDING MODEL WITH ANY LIGHTING
-//I BELIEVE THE CAUSE TO BE THE WAY MODEL WAS EXPORTED POSSIBLY
-//WOULD HAVE BEEN NICE AS HDRI WOULD GIVE REALISTIC LIGHTING TO SCENE
-
-// const loaderTexture = new RGBELoader();
-// loaderTexture.load(hdrTextureURL, function(texture){
-//   texture.mapping = THREE.EquirectangularReflectionMapping;
-//   scene.background = texture;
-// })
-
+//LIGHTING VARIABLES
+//OUTSIDE LIGHTING
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.015);
 scene.add(ambientLight);
-
-
 
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
 scene.add(directionalLight);
@@ -81,90 +102,58 @@ scene.add(dLightHelper);
 const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
 scene.add(dLightShadowHelper);
 
-
-//IN HOUSE LIGHTS
+//INTERNAL LIGHTS
 const directLight1 = new THREE.RectAreaLight(0xFFFFFF, 20, 2, 6);
-// scene.add(directLight1)
 directLight1.position.set(-29, 12, 49.7);
 directLight1.rotation.x = 0;
 directLight1.lookAt(-29, 0, 49.7);
 
 const directLight2 = new THREE.RectAreaLight(0xFFFFFF, 10, 6, 2);
-// scene.add(directLight1)
 directLight2.position.set(-26.5, 12, 67.5);
 directLight2.rotation.x = 0;
 directLight2.lookAt(-26.5, 0, 67.5);
+
+
+//HELPERS
+const axesHelper = new THREE.AxesHelper(5);
 
 const helper1 = new RectAreaLightHelper( directLight1 );
 const helper2 = new RectAreaLightHelper( directLight2 );
 
 
-
 //PARTICLES
-// Create a buffer geometry for the particles
 const particleGeometry = new THREE.BufferGeometry();
-
-// Create arrays to hold the particle positions
 const positions = [];
 
 // Add random positions to the particle positions array
 const numParticles = 500;
 for (let i = 0; i < numParticles; i++) {
-  const x = Math.random() * 400 - 200; // Random x position (-100 to 100)
-  const y = Math.random() * 400 - 200; // Random y position (-100 to 100)
-  const z = Math.random() * 800 - 400; // Random z position (-100 to 100)
-
+  const x = Math.random() * 400 - 200;
+  const y = Math.random() * 400 - 200;
+  const z = Math.random() * 800 - 400;
   positions.push(x, y, z);
 }
-
-// Create a Float32Array from the particle positions array
 const positionsArray = new Float32Array(positions);
 
 // Set the positions as an attribute in the buffer geometry
 particleGeometry.setAttribute('position', new THREE.BufferAttribute(positionsArray, 3));
-
-// Create a material for the particles
 const particleMaterial = new THREE.PointsMaterial({
-  size: 1,                    // Size of the particles
-  color: 0xffffff,            // Color of the particles
+  size: 1,                    
+  color: 0xffffff,            
   transparent: true,
-  opacity: 0.5,               // Opacity of the particles
-  // map: new THREE.TextureLoader().load('path/to/your/particleTexture.png'), // Texture for the particles (optional)
+  opacity: 0.5,
 });
 
 // Create a particle system with the buffer geometry and material
 const particles = new THREE.Points(particleGeometry, particleMaterial);
 
-// Add the particle system to the scene
+//ADDING ITEMS TO 3D SCENE
 scene.add(particles);
-
-// Set the light's target position
-// directLight1.target.position.copy(targetPosition);
-
-// Add the light to the scene
+scene.add(axesHelper);
 scene.add(directLight1, directLight2);
 // scene.add( helper1, helper2 );
 
 
-// const dLightHelper1 = new THREE.PointLightHelper(directLight1, 1);
-// scene.add(dLightHelper1);
-
-// const dLightShadowHelper1 = new THREE.CameraHelper(directLight1.shadow.camera);
-// scene.add(dLightShadowHelper1);
-// directionalLight.shadow.bias = -0.003;
-
-
-
-
-//VARIABLES
-var model = null;
-var start = { x: 0, y: 0 };
-var counter = 0;
-var currentIndex = 0;
-
-var childMesh = null;
-const clock = new THREE.Clock();
-const assetLoader = new GLTFLoader();
 assetLoader.load(buildingURL.href, function (gltf) {
   model = gltf.scene;
   console.log('MODEL', model);
@@ -194,33 +183,7 @@ assetLoader.load(buildingURL.href, function (gltf) {
   console.log(error)
 });
 
-const pointSets = [
-  {
-    startPoint: new THREE.Vector3(235, 162, 370),
-    endPoint: new THREE.Vector3(36.56, 12, 48.96),
-  },
-  {
-    startPoint: new THREE.Vector3(36.56, 12, 48.96),
-    endPoint: new THREE.Vector3(-11, 8, 49),
-    step: 50
-  },
-  {
-    startPoint: new THREE.Vector3(-11, 8, 49),
-    endPoint: new THREE.Vector3(-18, 7.7, 49.86),
-    step: 50
-  },
-  {
-    startPoint: new THREE.Vector3(-18, 7.7, 49.86),
-    endPoint: new THREE.Vector3(-30.8, 7.7, 55.2),
-    step: 50
-  },
-  {
-    startPoint: new THREE.Vector3(-30.8, 7.7, 55.2),
-    endPoint: new THREE.Vector3(-30, 7.4, 59.8),
-    
-  },
-  // Add more sets as needed
-];
+
 
 function raycast(){
   const raycaster = new THREE.Raycaster();
@@ -246,24 +209,20 @@ function raycast(){
   window.addEventListener('click', onMouseMove);
 }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
 
-//FOR DEBUGGING
-window.addEventListener('click', function () {
-  console.log(camera.position);
-  console.log(camera.rotation);
-});
-raycast();
-window.addEventListener('wheel', handleMouseWheel);
-window.addEventListener('touchstart', touchStart, false);
-window.addEventListener('touchmove', touchMove, false);
+
+//FOR DEBUGGING CAMERA POSITION AND ROTATION
+// window.addEventListener('click', function () {
+//   console.log(camera.position);
+//   console.log(camera.rotation);
+// });
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
 
 function touchStart(event) {
   start.x = event.touches[0].pageX;
@@ -286,12 +245,6 @@ function touchMove(event) {
     handleMouseWheel(deltaEvent);
   }
 }
-gsap.registerPlugin(MotionPathPlugin);
-
-const curve = new THREE.CatmullRomCurve3([
-  new THREE.Vector3(235, 162, 370),
-  new THREE.Vector3(36.56, 12.34, 48.96),
-]);
 
 function interpolation(){
   // const numPoints = 20;
@@ -321,8 +274,14 @@ function plottingCubesToPath(){
   });
 }
 
-interpolation();
-plottingCubesToPath();
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function handleMouseWheel(event) {
   var deltaY = event.deltaY;
@@ -410,30 +369,28 @@ function animateCameraBackward() {
   }
 }
 
-const points = curve.getPoints(50);
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-const curveObject = new THREE.Line(geometry, material);
-scene.add(curveObject);
-
-
-const stats = new Stats();
-document.body.appendChild(stats.dom);
-
-
 // Animate the scene
-function animate() {
-  requestAnimationFrame(animate);
+function animateScene() {
+  requestAnimationFrame(animateScene);
   renderer.render(scene, camera);
   stats.update();
+
+  //CAMERA CONTROLS FOR DEBUGGING PURPOSES
   // controls.update(clock.getDelta())
 }
-animate();
 
+//ADDING STATS PERFORMANCE ANALYTICS
+document.body.appendChild(stats.dom);
+
+//EVENT LISTENERS
+raycast();
+window.addEventListener('wheel', handleMouseWheel);
+window.addEventListener('touchstart', touchStart, false);
+window.addEventListener('touchmove', touchMove, false);
 window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.render(scene, camera);
-}
+
+interpolation();
+plottingCubesToPath();
+animateScene();
+
+
